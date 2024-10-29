b44ccccf970eef0a6599b68bf6a600042aaa6cc6
h3cth0r <hectorsucre13@gmail.com>
2024-10-29 12:16:34 -0600

    environment shell nix setup

---
+++ shell.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.python311
+    pkgs.python311Packages.numpy
+    pkgs.python311Packages.pandas
+    pkgs.python311Packages.matplotlib
+    pkgs.python311Packages.yfinance
+  ];
+
+  shellHook = ''
+    echo "Development environment with Python 3.9, numpy, yfinance, pandas, and matplotlib is ready."
+  '';
+}
